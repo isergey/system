2aa739085a889b35ae77421d9d3012484817f533
Sergey <dostovalov@gmail.com>
2012-02-03 17:06:28 +0400

    reginlib tag fix

--- apps/reginlib/templatetags/reginlib_tags.py
+++ apps/reginlib/templatetags/reginlib_tags.py
@@ -2,12 +2,13 @@
 from reginlib.models import RegistrationManager
 
 from django import template
-
+from django.contrib.auth.models import User
 register = template.Library()
 
 @register.filter
 def is_reg_manager(user):
-    count = RegistrationManager.objects.filter(user=user).count()
-    if count:
-        return True
+    if isinstance(user, User) and user.is_authenticated():
+        count = RegistrationManager.objects.filter(user=user).count()
+        if count:
+            return True
     return False
