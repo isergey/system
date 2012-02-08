8e254b43ab8447756f3824ec87c83a4ecff876a9
Sergey <dostovalov@gmail.com>
2012-02-08 19:25:52 +0400

    helper improve

--- apps/helper/views.py
+++ apps/helper/views.py
@@ -1,13 +1,19 @@
 # encoding: utf-8
 import sunburnt
 import simplejson as json
+from django.conf import settings
 from django.shortcuts import render, HttpResponse
 
 
 def index(request):
     answer = {'answer':u'Какая-то борода...'}
     if request.method == 'POST':
-        si = sunburnt.SolrInterface('http://127.0.0.1:8983/solr/')
+        SOLR_ADDRESS = settings.HELPER_SOLR_ADDRESS
+        try:
+            si = sunburnt.SolrInterface(SOLR_ADDRESS)
+        except AttributeError:
+            answer = json.dumps({'answer':u'Я заболел и не могу сейчас разговаривать :-('}, ensure_ascii=False)
+            return HttpResponse(answer)
 
         query = None
         terms = request.POST.get('ask', None)
@@ -17,7 +23,7 @@ def index(request):
             query = si.Q()
 
         for term in terms:
-            query = (query | si.Q(text_ru=term))
+            query = (query | si.Q(ask_t_ru=term))
 
         results = None
         if query:
