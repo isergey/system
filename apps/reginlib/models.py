7a4d47ceda9be3ff709a1e03437f833e09b435aa
Sergey <dostovalov@gmail.com>
2012-05-12 15:35:16 +0400

    fix

--- apps/reginlib/models.py
+++ apps/reginlib/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from participants.models import Library
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.core.mail import send_mail
 
 REGISTRATION_STATUSES = (
     (0, u'ожидание обработки'),
